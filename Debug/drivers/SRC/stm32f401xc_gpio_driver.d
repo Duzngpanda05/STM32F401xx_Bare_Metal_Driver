d6e62c36d51ba7c5819038725fc722492bd4531d
Duzngpanda05 <nguyenquangdung.tn05@gmail.com>
2026-03-24 10:13:20 +0700

    Remove IDE metadata and build files

--- Debug/drivers/SRC/stm32f401xc_gpio_driver.d
+++
@@ -1,6 +0,0 @@
-drivers/SRC/stm32f401xc_gpio_driver.o: \
- ../drivers/SRC/stm32f401xc_gpio_driver.c \
- D:/STM32_Workspace/stm32f4xx_drivers/drivers/INC/stm32f401xc_gpio_driver.h \
- D:/STM32_Workspace/stm32f4xx_drivers/drivers/INC/stm32f401xx.h
-D:/STM32_Workspace/stm32f4xx_drivers/drivers/INC/stm32f401xc_gpio_driver.h:
-D:/STM32_Workspace/stm32f4xx_drivers/drivers/INC/stm32f401xx.h:
